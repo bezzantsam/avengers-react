0154dd0741b2da671f821a660eb4539d84b5b6a1
Sam Bezzant <sambezzant@Sams-MacBook-Pro.local>
2021-12-12 04:58:01 -0800

    changes to App

--- App.js
+++ App.js
@@ -1,17 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
-import App from "./App";
-import { Route } from "react-router-dom";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import AvengersList from "./Components/AvengersList";
 import Home from "./Components/Home";
 import "./styles.css";
 
-ReactDOM.render(
-  <Router>
-    <Route exact path="/" component={Home} />
-    <Route path="/avengers" component={AvengerList} />
-    <App />
-  </Router>,
-  document.getElementById("root")
-);
+const App = () => {
+  return (
+    <Router>
+      <nav className="navbar">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/avengers">Avengers</Link>
+        </li>
+      </nav>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/avengers" component={AvengerList} />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
